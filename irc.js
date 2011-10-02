94ecd31dae328a2546f25919fe594a1c702597e9
echoline <echoline@gmail.com>
2011-10-02 15:15:48 -0400

    mathjax, formatting

--- irc.js
+++ irc.js
@@ -32,9 +32,123 @@ function begin() {
 	}
 }
 
+function valid(id) {
+	return id.replace(/[^A-Za-z0-9\-\_\:\.]/g, '');
+}
+
+function irc2html(str) {
+	formatting = {
+			bold:false,
+			underlined:false,
+			italics:false,
+	};
+	
+	str = str.replace(new RegExp("\u0003([0-9\,]+)([^\u0003\u000F]*)","g"), function(m, one, two) {
+					var ns;
+					var cs = [ "red", "white" ];
+
+					if (one.match(","))
+						ns = one.split(",");
+					else if (one.length != 0)
+						ns = [ one, "0" ];
+					else
+						return two;
+
+					for (i = 0; i < 2; i++) switch(ns[i]) {
+					case '0':
+						cs[i] = 'white';
+						break;
+					case '1':
+						cs[i] = 'black';
+						break;
+					case '2':
+						cs[i] = 'blue';
+						break;
+					case '3':
+						cs[i] = 'green';
+						break;
+					case '4':
+						cs[i] = 'pink';
+						break;
+					case '5':
+						cs[i] = 'red';
+						break;
+					case '6':
+						cs[i] = 'purple';
+						break;
+					case '7':
+						cs[i] = 'orange';
+						break;
+					case '8':
+						cs[i] = 'yellow';
+						break;
+					}
+
+					return "<span style=\"color:" + cs[0] + ";background-color:" + cs[1] + ";\">" + two + "</span>";
+				    });
+
+	str = str.replace(new RegExp("\u0003","g"), "");
+
+	str = str.replace(new RegExp("\u0002","g"), function() {
+					var ret = "\0002";
+
+					if (formatting.bold)
+						ret = "</strong>";
+					else
+						ret = "<strong>";
+
+					formatting.bold = formatting.bold? false: true;
+
+					return ret;
+				    });
+	
+	str = str.replace(new RegExp("\u001F","g"), function() {
+					var ret = "\001F";
+
+					if (formatting.underlined)
+						ret = "</span>";
+					else
+						ret = "<span style=\"text-decoration: underline;\">";
+
+					formatting.underlined = formatting.underlined? false: true;
+
+					return ret;
+				    });
+	
+	str = str.replace(new RegExp("\u0016","g"), function() {
+					var ret = "\0016";
+
+					if (formatting.italics)
+						ret = "</em>";
+					else
+						ret = "<em>";
+
+					formatting.italics = formatting.italics? false: true;
+
+					return ret;
+				    });
+
+	str = str.replace(new RegExp("\u000F","g"), function() {
+					var ret = "";
+
+					if (formatting.italics)
+						ret += "</em>";
+					if (formatting.bold)
+						ret += "</strong>";
+					if (formatting.underlined)
+						ret += "</span>";
+
+					formatting.italics = formatting.bold = formatting.underlined = false;
+
+					return ret;
+				    });
+
+	return str;
+}
+
 function msg(name) {
 	exists(name);
-	var id = String.fromCharCode(35).concat(name);
+	var id = String.fromCharCode(35).concat(valid(name));
 	$("#content").tabs("select", id);
 
 	return false;
@@ -46,20 +160,15 @@ var s = "";
 
 function exists(t) {
 	var ret = false;
-	var id;
 
-	$('.tabs li a').each(function(i) {
+	$('#tabs li a').each(function(i) {
 		if (this.text == t) {
 			ret = true;
 		}
 	});
 
 	if (!ret){
-		if (t.charAt(0).match(/^#$/)) {
-			id = t;
-		} else {
-			id = String.fromCharCode(35).concat(t);
-		}
+		var id = String.fromCharCode(35).concat(valid(t));
 
 		$('#content').tabs("add", id, t);
 	}
@@ -97,44 +206,46 @@ function load() {
 								t = from;
 
 							var m = s.substr(s.substr(1).indexOf(':') + 2).replace(/(https?):\/\/(\S+)/g, "<a href=\"$1://$2\">$1://$2</a>");
-							var id;
-						
-							if (t.charAt(0).match(/^#$/)) {
-								id = t;
-							} else {
-								id = String.fromCharCode(35).concat(t);
-							}
+							var id = String.fromCharCode(35).concat(valid(t));
 
 							// privmsg ctcps
 							if (m.charAt(0).match(/\001/)) {
 								if (m.substr(1).match(/^action/i)) {
+									exists(t);
+
 									m = m.replace(/^\001action(.*)\001$/i, "$1");
 									if (m.match(nick))
-										$(id).append("<span style=\"background:yellow;\">" + (new Date()).toLocaleTimeString() + " * " + from + " " + m + '</span><br/>');
+										$(id).append("<span style=\"background:yellow;\">" + (new Date()).toLocaleTimeString() + " * " + from + " " + irc2html(m) + '</span><br/>');
 									else
-										$(id).append((new Date()).toLocaleTimeString() + " * " + from + " " + m + '<br/>');
-								} else if (m.substr(1).match(/^version\001$/i)) {
+										$(id).append((new Date()).toLocaleTimeString() + " * " + from + " " + irc2html(m) + '<br/>');
+								} else if (m.match(/^\001version\001$/i)) {
 									document.getElementById('IRCSocket').write('NOTICE ' + from + ' :\001VERSION audreyirc v0.1\001\n');
-									
+
 								}
 							} else if (!id.match(/^#$/)) {
 								exists(t);
 
 								if (m.match(nick))
-									$(id).append("<span style=\"background:yellow;\">" + (new Date()).toLocaleTimeString() + " &lt;" + from + "&gt; " + m + '</span><br/>');
+									$(id).append("<span style=\"background:yellow;\">" + (new Date()).toLocaleTimeString() + " &lt;" + from + "&gt; " + irc2html(m) + '</span><br/>');
 								else
-									$(id).append((new Date()).toLocaleTimeString() + " &lt;" + from + "&gt; " + m + '<br/>');
+									$(id).append((new Date()).toLocaleTimeString() + " &lt;" + from + "&gt; " + irc2html(m) + '<br/>');
 
 								if (tablist[$('#content').tabs('option', 'selected')] && tablist[$("#content").tabs('option','selected')].text != t) {
-									$('.tabs li a').each(function(i) {
+									$('#tabs li a').each(function(i) {
 										if (this.text == t) {
 											$(this).css("color","#F00");
+											if (m.match(nick))
+												$(this).parent().effect("bounce", { times:3 }, 500);
 										}
 									});
 								}
+
 							} else {
 								$('#status').append(s + '<br/>');
 							}
+
+							if (MathJax)
+								MathJax.Hub.Queue(["Typeset",MathJax.Hub, "content"]);
 
 							window.scrollTo(0,document.body.scrollHeight);
 						} else if (list[1].match(/^353$/)) {
@@ -144,16 +255,16 @@ function load() {
 							var ppl = l.split(/\s+/);
 
 							exists(t);
-							$(t + ' .userlist').remove();
-							$(t).prepend('<ul class="userlist"></ul>');
+							var id = String.fromCharCode(35).concat(valid(t));
+							$(id + ' .userlist').remove();
+							$(id).prepend('<ul class="userlist"></ul>');
 							for (i in ppl) {
 								if (ppl[i].length > 0) {
 									var n = ppl[i].replace(/^([\~\@\%\+]|\&amp;)/,""); 
-									$(t + ' .userlist').prepend('<li><a href="#" onclick="return msg(this.innerHTML)">' + n + '</a></li>');
+									$(id + ' .userlist').prepend('<li><a href="#" onclick="return msg(this.innerHTML)">' + n + '</a></li>');
 								}
 							}
 
-							$(t + ' .userlist').menu();
 							window.scrollTo(0,document.body.scrollHeight);
 
 						} else if (list[1].match(/^332$/)) {
@@ -162,7 +273,8 @@ function load() {
 							l = l.substr(l.indexOf(':') + 1);
 
 							exists(t);
-							$(t).append(t + ": " + l + "<br/>");
+							var id = String.fromCharCode(35).concat(valid(t));
+							$(id).append((new Date()).toLocaleTimeString() + " " + l + "<br/>");
 							window.scrollTo(0,document.body.scrollHeight);
 
 						} else if (list[1].match(/nick/i)) {
@@ -175,35 +287,43 @@ function load() {
 								}
 							});
 
+							$('#tabs li a').each(function(i) {
+								if (this.innerHTML == from) {
+									this.innerHTML = to;
+								}
+							});
+
 							if (nick == from)
 								nick = to;
 
 						} else if (list[1].match(/join/i)) {
 							var who = list[0].substr(1,list[0].indexOf('!')-1);
 							var chan = list[2].substr(1);
+							var id = String.fromCharCode(35).concat(valid(chan));
 
-							$(chan + ' .userlist').prepend('<li><a href="#">' + who + '</a></li>');
-							$(chan + ' .userlist').menu("refresh");
-							$(chan).append((new Date()).toLocaleTimeString() + " " + who + " has joined " + chan + '<br/>');
+							$(id + ' .userlist').prepend('<li><a href="#" onclick="return msg(this.innerHTML)">' + who + '</a></li>');
+							$(id).append((new Date()).toLocaleTimeString() + " " + who + " has joined " + chan + '<br/>');
 							window.scrollTo(0,document.body.scrollHeight);
 
 						} else if (list[1].match(/part/i)) {
 							var who = list[0].substr(1,list[0].indexOf('!')-1);
 							var chan = list[2].substr(1);
+							var id = String.fromCharCode(35).concat(valid(chan));
 
-							$(chan + ' .userlist li').remove(":contains('" + who + "')");
-							$(chan).append((new Date()).toLocaleTimeString() + " " + who + " has left " + chan + '<br/>');
+							$(id + ' .userlist li').remove(":contains('" + who + "')");
+							$(id).append((new Date()).toLocaleTimeString() + " " + who + " has left " + chan + '<br/>');
 							window.scrollTo(0,document.body.scrollHeight);
 						
 						} else if (list[1].match(/kick/i)) {
 							var who = list[0].substr(1,list[0].indexOf('!')-1);
 							var chan = list[2];
+							var id = String.fromCharCode(35).concat(valid(chan));
 
-							$(chan + ' .userlist li').remove(":contains('" + list[3] + "')");
+							$(id + ' .userlist li').remove(":contains('" + list[3] + "')");
 							if (list[3].match(nick))
-								$(chan).append('<span style="color:#F00;">' + (new Date()).toLocaleTimeString() + ' you have been kicked from ' + chan + '</span><br/>');
+								$(id).append('<span style="color:#F00;">' + (new Date()).toLocaleTimeString() + ' you have been kicked from ' + chan + '</span><br/>');
 							else
-								$(chan).append((new Date()).toLocaleTimeString() + " " + who + " has kicked " + list[3] + ': ' + s.substr(s.substr(1).indexOf(':') + 2) + '<br/>');
+								$(id).append((new Date()).toLocaleTimeString() + " " + who + " has kicked " + list[3] + ': ' + s.substr(s.substr(1).indexOf(':') + 2) + '<br/>');
 
 							window.scrollTo(0,document.body.scrollHeight);
 						
@@ -211,8 +331,16 @@ function load() {
 							var who = list[0].substr(1,list[0].indexOf('!')-1);
 							var msg = s.substr(s.substr(1).indexOf(':') + 2);
 
-							$('.userlist li').remove(":contains('" + who + "')");
-							$(panellist[target]).append((new Date()).toLocaleTimeString() + " " + who + ' has quit: ' + msg + '<br/>');
+							$('.userlist:has(li)').each(function() {
+								$(this).find('li').each(function() {
+									if ((new RegExp("^" + who + "$").test($(this).text()))) {
+										$(this).parent().parent().append((new Date()).toLocaleTimeString() + " " + who + ' has quit: ' + msg + '<br/>');
+										$(this).remove();
+									}
+								});
+							});
+							$('#status').append((new Date()).toLocaleTimeString() + " " + who + ' has quit: ' + msg + '<br/>');
+
 							window.scrollTo(0,document.body.scrollHeight);
 
 						} else if (list[0].match(/^error/i)) {
@@ -236,7 +364,7 @@ function load() {
 			s = "";
 		}
 
-		setTimeout('load()', 10);
+		setTimeout('load()', 100);
 	} catch(e) {
 		document.getElementById('status').innerHTML += e;
 		window.scrollTo(0,document.body.scrollHeight);
@@ -247,13 +375,15 @@ function send(event) {
 	if (event.keyCode == 13) {
 		try {
 			var l = document.getElementById('inbox').value;
+			l = l.replace(/\\u(\d|[A-F]){4}/gi, function(arg) { return String.fromCharCode(parseInt(arg.substr(2), 16)); });
 			if (l.charAt(0) == '/') {
 				if (l.substr(1).match(/^me\ /i) && target) {
 					var tab = tablist[target];
+
 					document.getElementById('IRCSocket').write('privmsg ' + tab.text + ' :'  + l.replace(/^\/me\ (.*)$/i, "\001ACTION $1\001") + '\n');
-					l = l.replace(/^\/me\ /i,"").replace(/\&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/(https?):\/\/(\S+)/g, "<a href=\"$1://$2\">$1://$2</a>");
+					l = l.replace(/^\/me\ /i,"").replace(/\&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/(https?):\/\/(\S+)/g, "<a href=\"$1://$2\">$1://$2</a>")
 					var panel = panellist[target];
-					$(panel).append((new Date()).toLocaleTimeString() + " * <span style=\"color:#F00\">" + nick + "</span> " + l + '<br/>');
+					$(panel).append((new Date()).toLocaleTimeString() + " * <span style=\"color:#F00\">" + nick + "</span> " + irc2html(l) + '<br/>');
 
 				} else
 					document.getElementById('IRCSocket').write(l.substr(1) + '\n');
@@ -261,13 +391,15 @@ function send(event) {
 			} else if (target) {
 				var tab = tablist[target];
 				document.getElementById('IRCSocket').write('privmsg ' + tab.text + ' :'  + l + '\n');
-				l = l.replace(/\&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/(https?):\/\/(\S+)/g, "<a href=\"$1://$2\">$1://$2</a>");
+				l = l.replace(/\&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/(https?):\/\/(\S+)/g, "<a href=\"$1://$2\">$1://$2</a>").replace(/\\u([\d|A-F]{4})/gi, function(arg){return "" + String.fromCharCode(parseInt(arg, 16));});
 				var panel = panellist[target];
-				$(panel).append((new Date()).toLocaleTimeString() + " &lt;<span style=\"color:#F00\">" + nick + "</span>&gt; " + l + '<br/>');
+				$(panel).append((new Date()).toLocaleTimeString() + " &lt;<span style=\"color:#F00\">" + nick + "</span>&gt; " + irc2html(l) + '<br/>');
 			}
 
+			if (MathJax)
+				MathJax.Hub.Queue(["Typeset",MathJax.Hub, "content"]);
+
 			window.scrollTo(0,document.body.scrollHeight);
-			
 		} catch (e) {
 			document.getElementById('status').innerHTML += e;
 		}
